0a0dc67360ac42b94b9de8dbdfd07eb99eefd06c
Sz Sebas <sz.sebas@gmail.com>
2020-09-17 08:14:08 -0300

    refactoring

--- application/core/MY_Model.php
+++ application/core/MY_Model.php
@@ -6,6 +6,7 @@ abstract class MY_Model extends CI_Model  {
     public $idname = "";
 	public $order = "";
 	public $loggear = TRUE;
+	public $eliminarSeguro = "";
 
    function __construct() {
           parent::__construct();
@@ -225,7 +226,14 @@ abstract class MY_Model extends CI_Model  {
     }
 
     /*Para las DATATABLES*/
-    protected abstract function getWhere($params);
+    protected function getWhere($params){
+		$search = $params['busqueda'];
+		$where = "WHERE 1=1 ";
+		if(!empty($search)){
+			$where .= "AND (nombre like '%".$this->db->escape_like_str($search)."%') ";
+		}
+		return $where;
+	}
     protected function getWhereObligado(){
       return ''; 
     }
@@ -282,7 +290,22 @@ abstract class MY_Model extends CI_Model  {
 			return $id;
 		}
         return $resultado[0]->id;
-    }
+	}
+	
+	public function eliminarSeguro($id){
+		$query=$this->db->query($this->eliminarSeguro,$id);
+		$resultado=$query->result_array();
+		if(empty($resultado)){
+			$query=$this->db->query("DELETE FROM {$this->table}_lang WHERE {$this->idname}=?",$id);
+			$query=$this->db->query("DELETE FROM {$this->table} WHERE {$this->idname}=?",$id);
+			if($this->loggear){
+				$this->db->query("INSERT INTO Logs(entidad, sentencia, idusuario, idprincipal, funcion) VALUES(?,?,?,?,?) ", Array($this->table, "DELETE FROM {$this->table} WHERE {$this->idname}={$id}", $this->session->userdata('idusuario'), $id, 'eliminarSeguro') );
+			}
+			return '';
+		}else{
+			return 'No se puede eliminar un elemento en uso';
+		}
+	}
 
   }
 ?>
